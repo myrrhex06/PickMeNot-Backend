723016b9988a984adc2ec486610bf0f56d01e29d
myrrhex06 <gitlwer210@gmail.com>
2026-08-08 16:47:21 +0900

    Initial commit

---
+++ src/main/java/com/pick_me_not/PickMeNotApplication.java
@@ -0,0 +1,13 @@
+package com.pick_me_not;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickMeNotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickMeNotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pick_me_not/PickMeNotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pick_me_not;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickMeNotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
